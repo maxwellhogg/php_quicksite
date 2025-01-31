da38844f130ab755c72058db0dc6dc7b1b8c6db4
Maxwell Hogg <maxy.hogg@googlemail.com>
2025-01-31 16:08:04 +0000

    added more portfolio elements

--- includes/portfolio-elements/portfolio-section-3.php
+++ includes/portfolio-elements/portfolio-section-3.php
@@ -2,26 +2,38 @@
     <h2>My Projects</h2>
     <div class="portfolio-project-card-3">
         <img src="./images/project1.jpg" alt="">
-        <div class="portfolio-project-details">
-            <h3>Project 1</h3>
+        <div class="portfolio-project-details-3">
+            <h3>Project Title 1</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, natus possimus quam facilis eos odit pariatur nisi qui! Doloribus sunt excepturi exercitationem hic officia odio quae nulla eaque dolorum enim!</p>
-            <p><span>HTML</span><span>CSS</span><span>JS</span></p>
+            <p><span class="project-resources-3">HTML</span> <span class="project-resources-3">CSS</span> <span class="project-resources-3">JS</span></p>
+            <button class="project-button-3" src="#">SEE PROJECT</button>
         </div>
     </div>
     <div class="portfolio-project-card-3">
-    <img src="./images/project2.jpg" alt="">
-        <div class="portfolio-project-details">
-            <h3>Project 1</h3>
+        <img src="./images/project2.jpg" alt="">
+        <div class="portfolio-project-details-3">
+            <h3>Project Title 2</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, natus possimus quam facilis eos odit pariatur nisi qui! Doloribus sunt excepturi exercitationem hic officia odio quae nulla eaque dolorum enim!</p>
-            <p><span>HTML</span><span>CSS</span><span>JS</span></p>
+            <p><span class="project-resources-3">HTML</span> <span class="project-resources-3">CSS</span> <span class="project-resources-3">JS</span></p>
+            <button class="project-button-3" src="#">SEE PROJECT</button>
         </div>
     </div>
     <div class="portfolio-project-card-3">
-    <img src="./images/project1.jpg" alt="">
-        <div class="portfolio-project-details">
-            <h3>Project 1</h3>
+        <img src="./images/project1.jpg" alt="">
+        <div class="portfolio-project-details-3">
+            <h3>Project Title 3</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, natus possimus quam facilis eos odit pariatur nisi qui! Doloribus sunt excepturi exercitationem hic officia odio quae nulla eaque dolorum enim!</p>
-            <p><span>HTML</span><span>CSS</span><span>JS</span></p>
+            <p><span class="project-resources-3">HTML</span> <span class="project-resources-3">CSS</span> <span class="project-resources-3">JS</span></p>
+            <button class="project-button-3" src="#">SEE PROJECT</button>
         </div>
     </div>
+    <button class="more-projects-button-3" src="">SEE MORE PROJECTS</button>
 </section>
+
+<!-- 
+    var(--cultured);
+    var(--jade);
+    var(--cadmium-orange);
+    var(--deep-space-sparkle);
+    var(--rich-black-fogra39); 
+    -->
