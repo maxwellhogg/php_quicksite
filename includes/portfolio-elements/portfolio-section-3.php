849489507a1c5423eff677f454059be0bbf30cc7
Maxwell Hogg <maxy.hogg@googlemail.com>
2025-01-31 11:20:27 +0000

    Added more portfolio elements

---
+++ includes/portfolio-elements/portfolio-section-3.php
@@ -0,0 +1,27 @@
+<section class="portfolio-container-3">
+    <h2>My Projects</h2>
+    <div class="portfolio-project-card-3">
+        <img src="./images/project1.jpg" alt="">
+        <div class="portfolio-project-details">
+            <h3>Project 1</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, natus possimus quam facilis eos odit pariatur nisi qui! Doloribus sunt excepturi exercitationem hic officia odio quae nulla eaque dolorum enim!</p>
+            <p><span>HTML</span><span>CSS</span><span>JS</span></p>
+        </div>
+    </div>
+    <div class="portfolio-project-card-3">
+    <img src="./images/project2.jpg" alt="">
+        <div class="portfolio-project-details">
+            <h3>Project 1</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, natus possimus quam facilis eos odit pariatur nisi qui! Doloribus sunt excepturi exercitationem hic officia odio quae nulla eaque dolorum enim!</p>
+            <p><span>HTML</span><span>CSS</span><span>JS</span></p>
+        </div>
+    </div>
+    <div class="portfolio-project-card-3">
+    <img src="./images/project1.jpg" alt="">
+        <div class="portfolio-project-details">
+            <h3>Project 1</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, natus possimus quam facilis eos odit pariatur nisi qui! Doloribus sunt excepturi exercitationem hic officia odio quae nulla eaque dolorum enim!</p>
+            <p><span>HTML</span><span>CSS</span><span>JS</span></p>
+        </div>
+    </div>
+</section>
--- index.php
+++ index.php
@@ -14,18 +14,19 @@ include './includes/hero-elements/hero-centered.php';
 
 // about-us-elements
 include './includes/about-us-elements/about-us-1.php';
+// include './includes/about-us-elements/about-us-2.php';
+// include './includes/about-us-elements/about-us-3.php';
 include './includes/about-us-elements/about-us-2.php';
-include './includes/about-us-elements/about-us-3.php';
 
 // ------OFFER CARDS-----
-include './includes/offers-cards-elements/offers-cards-1.php';
+// include './includes/offers-cards-elements/offers-cards-1.php';
 include './includes/offers-cards-elements/offers-cards-2.php';
-include './includes/offers-cards-elements/offers-cards-3.php';
+// include './includes/offers-cards-elements/offers-cards-3.php';
 
 // ------PORTFOLIO-----
 include './includes/portfolio-elements/portfolio-section-1.php';
-include './includes/portfolio-elements/portfolio-section-2.php';
-
+// include './includes/portfolio-elements/portfolio-section-2.php';
+include './includes/portfolio-elements/portfolio-section-3.php';
 
 // -----FOOTER--------
 include './includes/footer.php'
